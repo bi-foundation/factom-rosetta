cec99545bb281e31546ab4021e2558273e1e9a5e
nklomp <nklomp@sphereon.com>
2020-07-29 12:56:09 +0200

    ROSS-1 Use binding for multiple network/client support

--- src/main/java/org/blockchain_innovation/factom/rosetta/api/AccountDelegate.java
+++ src/main/java/org/blockchain_innovation/factom/rosetta/api/AccountDelegate.java
@@ -1,4 +1,26 @@
 package org.blockchain_innovation.factom.rosetta.api;
 
+import org.blockchain_innovation.factom.client.api.FactomdClient;
+import org.blockchain_innovation.factom.rosetta.api.factom.client.ClientRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+import java.util.Optional;
+
 public class AccountDelegate {
+
+    private final Environment environment;
+    private final ClientRegistry registry;
+
+    @Autowired
+    public AccountDelegate(Environment environment, ClientRegistry registry) {
+        this.environment = environment;
+        this.registry = registry;
+    }
+
+
+    protected FactomdClient client(Optional<String> network) {
+        return registry.client(network);
+    }
+
 }
